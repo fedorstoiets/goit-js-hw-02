f35910452b4d284f664864959febc721c3111bdf
fedorstoiets <121324801+fedorstoiets@users.noreply.github.com>
2026-09-14 20:22:59 -0400

    Add files via upload

--- task-4.js
+++ task-4.js
@@ -1,21 +1,21 @@
 function getShippingCost(country) {
   switch (country) {
     case "China":
-      return `Shipping to ${country} will cost 100 credits`;
+      return `Доставка до ${country} коштуватиме 100 кредитів`;
     case "Chile":
-      return `Shipping to ${country} will cost 250 credits`;
+      return `Доставка до ${country} коштуватиме 250 кредитів`;
     case "Australia":
-      return `Shipping to ${country} will cost 170 credits`;
+      return `Доставка до ${country} коштуватиме 170 кредитів`;
     case "Jamaica":
-      return `Shipping to ${country} will cost 120 credits`;
+      return `Доставка до ${country} коштуватиме 120 кредитів`;
     default:
-      return "Sorry, there is no delivery to your country";
+      return "На жаль, доставка до вашої країни не здійснюється";
   }
 }
 
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("Australia")); // "Доставка до Australia коштуватиме 170 кредитів"
+console.log(getShippingCost("Germany")); // "На жаль, доставка до вашої країни не здійснюється"
+console.log(getShippingCost("China")); // "Доставка до China коштуватиме 100 кредитів"
+console.log(getShippingCost("Chile")); // "Доставка до Chile коштуватиме 250 кредитів"
+console.log(getShippingCost("Jamaica")); // "Доставка до Jamaica коштуватиме 120 кредитів"
+console.log(getShippingCost("Sweden")); // "На жаль, доставка до вашої країни не здійснюється"
